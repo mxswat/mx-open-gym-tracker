2502fadfc4fbda9512bb26e06ef78e2b5baea60c
mxswat <massimocacciapaglia.13@gmail.com>
2026-02-21 21:16:11 +0000

    Adds tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      buffer: 'buffer',
-      process: 'process/browser',
+      buffer: "buffer",
+      process: "process/browser",
     },
   },
   define: {
-    global: 'globalThis',
+    global: "globalThis",
   },
   optimizeDeps: {
-    include: ['buffer', 'process'],
+    include: ["buffer", "process"],
   },
 });
