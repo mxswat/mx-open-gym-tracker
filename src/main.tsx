4f5b6f9c02b4b58564ca8aeda9ac0acb86ee33cb
mxswat <massimocacciapaglia.13@gmail.com>
2026-02-21 22:12:55 +0000

    Hard copy pasting a generated boilerplate from running `pnpm create @vite-pwa/pwa`

--- src/main.tsx
+++ src/main.tsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { Buffer } from 'buffer';
-import process from 'process';
-import { routeTree } from './routeTree';
-import './styles/app.css';
-import { registerServiceWorker } from './services/offline';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-if (!globalThis.Buffer) {
-  globalThis.Buffer = Buffer;
-}
-
-if (!globalThis.process) {
-  globalThis.process = process;
-}
-
-const router = createRouter({
-  routeTree,
-  context: {
-    appName: 'MX Open Gym Tracker',
-  },
-  defaultPreload: 'intent',
-});
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
-
-registerServiceWorker();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
